2157594ee177876e732f13ef572e17c577c108e7
Phil Freeman <paf31@cantab.net>
2014-04-01 20:27:27 -0700

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  "use strict";
+
+  grunt.initConfig({ 
+  
+    libFiles: [
+      "src/**/*.purs",
+      "bower_components/purescript-*/src/**/*.purs",
+      "bower_components/purescript-*/src/**/*.purs.hs"
+    ],
+    
+    clean: {
+      tests: ["tmp"],
+      lib: ["js", "externs"]
+    },
+  
+    "purescript-make": {
+      lib: {
+        src: "<%=libFiles%>"
+      }
+    },
+  
+    purescript: {
+      tests: {
+        options: {
+          module: ["Main"],
+          main: true
+        },
+        src: ["tests/Tests.purs", "<%=libFiles%>"],
+        dest: "tmp/tests.js"
+      }
+    },
+    
+    execute: {
+      tests: {
+        src: "tmp/tests.js"
+      }
+    }      
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-purescript");
+  grunt.loadNpmTasks("grunt-execute");
+  
+  grunt.registerTask("test", ["clean:tests", "purescript:tests", "execute:tests"]);
+  grunt.registerTask("lib", ["purescript-make:lib"]);
+  grunt.registerTask("default", ["test", "lib"]);
+};
