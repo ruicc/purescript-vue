4be7b9ac1d957b974728ef86359927eb28e11f81
Ruichi Kousuke <kou.ruichi@altplus.co.jp>
2014-05-16 19:42:34 +0700

    Add a simple example.

--- src/Main.purs
+++ src/Main.purs
@@ -1,7 +1,22 @@
 module Main where
 
-import Foo
+import Control.Monad.Eff
 import Debug.Trace
 
-main = do
-  trace "Hello, World!"
+main = vue $ def { dat = { message: "Can you hear me?" } }
+
+foreign import vue
+    "function vue(opt) {\
+    \    return function() {\
+    \        if (opt.dat !== undefined) {\
+    \            opt.data = opt.dat;\
+    \            delete opt.dat;\
+    \        }\
+    \        new Vue(opt);\
+    \    }\
+    \}" :: forall e. VueOpt -> Eff e {}
+
+type VueOpt = { el :: String, dat :: { message :: String } }
+
+def :: VueOpt
+def = { el: "#demo" , dat: { message: "Hello Vue.js!" } }
